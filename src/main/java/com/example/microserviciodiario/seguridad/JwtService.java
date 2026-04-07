074885e9d29c2390ff5d73083838f481fa520c07
SantiagoCA <satiago.castrilloa@uqvirtual.edu.co>
2026-04-07 12:16:55 -0500

    feat: implement JWT service for authentication and global exception handler for error management

--- src/main/java/com/example/microserviciodiario/seguridad/JwtService.java
+++ src/main/java/com/example/microserviciodiario/seguridad/JwtService.java
@@ -3,12 +3,12 @@ package com.example.microserviciodiario.seguridad;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -75,7 +75,7 @@ public class JwtService {
     }
 
     private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
